44e6caa77ca9e3cc5313e0d1bbdd8cbfe99bf4b8
davidcoats <david@getsafetycone.com>
2023-04-24 00:22:04 -0700

    Interim changes.

--- source/D8S.R0001/Code/Instances.cs
+++ source/D8S.R0001/Code/Instances.cs
@@ -5,6 +5,6 @@ namespace D8S.R0001
 {
     public static class Instances
     {
-		
+        public static R5T.F0000.IDateTimeOperator DateTimeOperator => R5T.F0000.DateTimeOperator.Instance;
     }
 }
---
+++ source/D8S.R0001/Components/BlogArticleCard.razor.cs
@@ -0,0 +1,16 @@
+using System;
+
+using R5T.T0154;
+
+
+namespace D8S.W0003.R000
+{
+    /// <summary>
+    /// A card on the index page for a blog article.
+    /// </summary>
+    [RazorComponentMarker]
+    public partial class BlogArticleCard : IRazorComponentMarker
+    {
+
+    }
+}
